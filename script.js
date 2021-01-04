f085ae6e1ad11c99a00c583e1130df7e56eadc8a
Dennis Rathjen <o0shojo0o@dietru.de>
2021-01-04 23:03:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ////////// ToDo //////////
-//
+// 
 // - Colortemp handling
 // - Color handling
 // - NightTime handling 
@@ -8,32 +8,32 @@
 
 // Define actors
 const Actors = [
-    // eg. {name: '', id: '', on: '', level: '', ct: '', color: '', posMin: 0, posMax: 0},
+    // eg. {name: '', id: '', on: '', level: '', ct: '', color: '', posMin: 0, posMax: 100, briMin:0, briMax: 100},
     //Flur
     {name: 'FL.Licht.Decke.Hinten', id: 'zigbee.0.0017880102a096e5'},
     {name: 'FL.Licht.Decke.Vorne', id: 'zigbee.0.0017880102eebc0b'},
     {name: 'FL.Licht.Links', id: 'zigbee.0.84182600000c6dfd'},
     // Küche
-    {name: 'KU.Licht.Deckenleuchte', id: 'zigbee.0.group_1'},
-    {name: 'KU.Licht.Deckenleuchte.1', id: 'zigbee.0.842e14fffe2304a1'},
-    {name: 'KU.Licht.Deckenleuchte.2', id: 'zigbee.0.588e81fffee3b050'},
-    {name: 'KU.Licht.Deckenleuchte.3', id: 'zigbee.0.588e81fffed3eddd'},
+    {name: 'KU.Licht.Deckenleuchte', id: 'zigbee.0.group_1', briMin: 2},
+    {name: 'KU.Licht.Deckenleuchte.1', id: 'zigbee.0.842e14fffe2304a1', briMin: 2},
+    {name: 'KU.Licht.Deckenleuchte.2', id: 'zigbee.0.588e81fffee3b050', briMin: 2},
+    {name: 'KU.Licht.Deckenleuchte.3', id: 'zigbee.0.588e81fffed3eddd', briMin: 2},
     {name: 'KU.Licht.Arbeitsplatte', id: 'zigbee.0.group_3'},
     {name: 'KU.Licht.Arbeitsplatte.Links',  id: 'zigbee.0.7cb03eaa00b1a4bf'},
     {name: 'KU.Licht.Arbeitsplatte.Rechts', id: 'zigbee.0.84182600000c9c50'},
     // Wohnzimmer
     {name: 'WZ.Licht.Fensterbank', id: 'zigbee.0.84182600000c77dd'},
-    {name: 'WZ.Licht.StandLampe', id: 'zigbee.0.588e81fffedb8b1a'},    
+    {name: 'WZ.Licht.StandLampe', id: 'zigbee.0.588e81fffedb8b1a', briMin: 2},    
     // Kinderzimmer
     {name: 'KZ.Licht.Betthimmel', id: 'zigbee.0.8418260000101f8c'},
     {name: 'KZ.Licht.Disco', id: 'zigbee.0.841826000010328a'},
-    {name: 'KZ.Licht.Decke', id: 'zigbee.0.842e14fffe1f08e7'},   
+    {name: 'KZ.Licht.Decke', id: 'zigbee.0.842e14fffe1f08e7', briMin: 2},   
     // Schlafzimmer
     {name: 'SZ.Licht.Bett', id: 'zigbee.0.588e81fffefea724'},
     {name: 'SZ.Blind.Links', id: 'zigbee.0.680ae2fffe974af1', posMin: 0, posMax: 90},
     {name: 'SZ.Blind.Rechts', id: 'zigbee.0.680ae2fffe54a2ba', posMin: 0, posMax: 90},
     // Terrasse
-    {name: 'TR.Licht.GardenPole', id: 'zigbee.0.7cb03eaa00a9572e'},   
+    {name: 'TR.Licht.GardenPole', id: 'zigbee.0.7cb03eaa00a9572e', briMin: 2},   
     // Büro
     {name: 'BU.Blind', id: 'zigbee.0.680ae2fffeed55f6', posMin: 0, posMax: 97},
 ];
@@ -42,8 +42,8 @@ const Actors = [
 const Sensors = [
     // eg. {name: '', id: ''},
     //Flur
-    {name: 'FL.Licht.Decke.Hinten', id: ''}, // Todo
-    {name: 'FL.Licht.Decke.Vorne', id: ''}, // Todo
+    {name: 'FL.Sensor.Taster.UP.Hinten', id: ''}, // Todo
+    {name: 'FL.Sensor.Taster.UP.Vorne', id: ''}, // Todo
     // Küche
     {name: 'KU.Sensor.Taster', id: 'zigbee.0.00158d00027bd7e1'},
     // Büro
@@ -54,83 +54,116 @@ const Sensors = [
     {name: 'KZ.Sensor.Taster.UP', id: 'zigbee.0.5c0272fffe3c2124'},
     {name: 'KZ.Sensor.Fernbedienung', id: 'zigbee.0.ec1bbdfffea53484'},
     // Wohnzimmer
-    {name: 'WZ.Sensor.Taster.Dimmer', id: 'zigbee.0.00178801080cf9f9'},
+    {name: 'WZ.Sensor.Fernbedienung', id: 'zigbee.0.00178801080cf9f9'},
     // Schlafzimmer
     {name: 'SZ.Sensor.Taster.OpenClose', id: 'zigbee.0.680ae2fffe549bb6'},   
     
 ];
  
-// Define events
+// Definde events
 const Events = [
-    // eg.  {sensor: '', event: '', actors: ['',''], action: ''}, current available [ toggle, on, off, autoDim ]
+    // eg.  {sensor: '', events: ['',''], actors: ['',''], action: ''}, current available [ toggle, on, off, autoDim, dimUp, dimDown, open, close ]
     // Küche
-    {sensor: 'KU.Sensor.Taster', event: 'right_click', actors: ['KU.Licht.Arbeitsplatte.Links'], action: 'toggle'},
-    {sensor: 'KU.Sensor.Taster', event: 'left_click', actors: ['KU.Licht.Arbeitsplatte.Rechts'], action: 'toggle'},
-    {sensor: 'KU.Sensor.Taster', event: 'both_click', actors: ['KU.Licht.Deckenleuchte'], action: 'toggle'},
+    {sensor: 'KU.Sensor.Taster', events: ['right_click'], actors: ['KU.Licht.Arbeitsplatte.Links'], action: 'toggle'},
+    {sensor: 'KU.Sensor.Taster', events: ['left_click'], actors: ['KU.Licht.Arbeitsplatte.Rechts'], action: 'toggle'},
+    {sensor: 'KU.Sensor.Taster', events: ['both_click'], actors: ['KU.Licht.Deckenleuchte'], action: 'toggle'},
     // Kinderzimmer
-    {sensor: 'KZ.Sensor.Taster', event: 'left_click', actors: ['KZ.Licht.Disco'], action: 'toggle'},
-    {sensor: 'KZ.Sensor.Taster', event: 'right_click', actors: ['KZ.Licht.Betthimmel'], action: 'toggle'},
-    {sensor: 'KZ.Sensor.Taster.UP', event: 'state', actors: ['KZ.Licht.Decke'], action: 'toggle'},
-    {sensor: 'KZ.Sensor.Taster.UP', event: 'up_button', actors: ['KZ.Licht.Decke'], action: 'autoDim'},
+    {sensor: 'KZ.Sensor.Taster', events: ['left_click'], actors: ['KZ.Licht.Disco'], action: 'toggle'},
+    {sensor: 'KZ.Sensor.Taster', events: ['right_click'], actors: ['KZ.Licht.Betthimmel'], action: 'toggle'},
+    {sensor: 'KZ.Sensor.Taster.UP', events: ['state'], actors: ['KZ.Licht.Decke'], action: 'toggle'},
+    {sensor: 'KZ.Sensor.Taster.UP', events: ['up_button'], actors: ['KZ.Licht.Decke'], action: 'autoDim'},
+    //{sensor: 'KZ.Sensor.Fernbedienung', events: ['on'], actors: ['KZ.Licht.Decke'], action: 'on'},
+    //{sensor: 'KZ.Sensor.Fernbedienung', events: ['off'], actors: ['KZ.Licht.Decke'], action: 'off'},
+    //{sensor: 'KZ.Sensor.Fernbedienung', events: ['brightness_step_up'], actors: ['KZ.Licht.Decke'], action: 'dimUp'},
+    //{sensor: 'KZ.Sensor.Fernbedienung', events: ['brightness_step_down'], actors: ['KZ.Licht.Decke'], action: 'dimDown'},
     // Schlafzimmer
-    {sensor: 'SZ.Sensor.Taster.OpenClose', event: 'cover_open', actors: ['SZ.Blind.Links', 'SZ.Blind.Rechts'], action: 'open'},
-    {sensor: 'SZ.Sensor.Taster.OpenClose', event: 'cover_close', actors: ['SZ.Blind.Links', 'SZ.Blind.Rechts'], action: 'close'},
+    {sensor: 'SZ.Sensor.Taster.OpenClose', events: ['cover_open'], actors: ['SZ.Blind.Links', 'SZ.Blind.Rechts'], action: 'open'},
+    {sensor: 'SZ.Sensor.Taster.OpenClose', events: ['cover_close'], actors: ['SZ.Blind.Links', 'SZ.Blind.Rechts'], action: 'close'},
     // Büro
-    {sensor: 'BU.Sensor.Taster.OpenClose', event: 'cover_open', actors: ['BU.Blind'], action: 'open'},
-    {sensor: 'BU.Sensor.Taster.OpenClose', event: 'cover_close', actors: ['BU.Blind'], action: 'close'},
+    {sensor: 'BU.Sensor.Taster.OpenClose', events: ['cover_open'], actors: ['BU.Blind'], action: 'open'},
+    {sensor: 'BU.Sensor.Taster.OpenClose', events: ['cover_close'], actors: ['BU.Blind'], action: 'close'},
+    // Wohnzimmer
+    {sensor: 'WZ.Sensor.Fernbedienung', events: ['state'], actors: ['WZ.Licht.StandLampe'], action: 'onOff'},
+    {sensor: 'WZ.Sensor.Fernbedienung', events: ['up_button', 'up_hold'], actors: ['WZ.Licht.StandLampe'], action: 'dimUp'},
+    {sensor: 'WZ.Sensor.Fernbedienung', events: ['down_button', 'down_hold'], actors: ['WZ.Licht.StandLampe'], action: 'dimDown'},
 ];
 
 /////////////////////////////////////////////////// Logic ////////////////////////////////////////////////////////////////////
-//
 const cache = [];
 
 // Register events
+// Run through each definetion
 for (const key in Events) {
     const sensor = Sensors.find(x=>x.name == Events[key].sensor);
-    const triggerDp = sensor.id + '.' + Events[key].event;    
-    const actors = Events[key].actors;
-    const action = Events[key].action;
+    const events = Events[key].events;   
+    // Run through each events
+    for (const eKey in events) {
+        const triggerDp = sensor.id + '.' + events[eKey];    
+        const actors = Events[key].actors;
+        const action = Events[key].action;
+        
+        // Toogle event register
+        if (action == 'toggle') {
+            on ({id: triggerDp, val: true}, (obj)=> {
+                toggle(actors);
+            });
+        }
     
-    // Toogle event register
-    if (action == 'toggle') {
-        on ({id: triggerDp, val: true}, (obj)=> {
-            toggle(actors);
-        });
-    }
-   
-    // On event register
-    else if (action == 'on') {
-        on ({id: triggerDp, val: true}, (obj)=> {
-            setOnOff(actors, true);
-        });
-    } 
-    
-    // Off event register
-    else if (action == 'off') {
-        on ({id: triggerDp, val: true}, (obj)=> { 
-            setOnOff(actors, false);
-        });
-    }
+        // On event register
+        else if (action == 'on') {
+            on ({id: triggerDp, val: true}, (obj)=> {
+                setOnOff(actors, true);
+            });
+        } 
+        
+        // Off event register
+        else if (action == 'off') {
+            on ({id: triggerDp, val: true}, (obj)=> { 
+                setOnOff(actors, false);
+            });
+        }
 
-    // AutoDim event register
-    else if (action == 'autoDim') {
-        on ({id: triggerDp}, (obj)=> { 
-            autoDim(obj, actors);
-        });
-    }
+        // OnOff event register
+        else if (action == 'onOff') {
+            on ({id: triggerDp}, (obj)=> { 
+                setOnOff(actors, obj.state.val);
+            });
+        }
 
-    // Open (Blind) event register
-    else if (action == 'open') {
-        on ({id: triggerDp, val: true}, (obj)=> { 
-            openClose(actors, true);
-        });
-    }
+        // AutoDim event register
+        else if (action == 'autoDim') {
+            on ({id: triggerDp}, (obj)=> { 
+                autoDim(obj, actors);
+            });
+        }
 
-    // Close (Blind) event register
-    else if (action == 'close') {
-        on ({id: triggerDp, val: true}, (obj)=> { 
-            openClose(actors, false);
-        });
+        // DimUp event register
+        else if (action == 'dimUp') {
+            on ({id: triggerDp, val: true}, (obj)=> { 
+                dimUpDown(actors, true);
+            });
+        }
+
+        // DimDown event register
+        else if (action == 'dimDown') {
+            on ({id: triggerDp, val: true}, (obj)=> { 
+                dimUpDown(actors, false);
+            });
+        }
+
+        // Open (Blind) event register
+        else if (action == 'open') {
+            on ({id: triggerDp, val: true}, (obj)=> { 
+                openClose(actors, true);
+            });
+        }
+
+        // Close (Blind) event register
+        else if (action == 'close') {
+            on ({id: triggerDp, val: true}, (obj)=> { 
+                openClose(actors, false);
+            });
+        }
     }
 }
 
@@ -225,35 +258,55 @@ function toggle(actors) {
 * @param {string[]} actors
 */
 function autoDim(obj, actors){
+    // Check if the cache is available, if not one will be created 
     const cacheKey = JSON.stringify(actors);
     if (!cache[cacheKey]){
         cache[cacheKey] = { dimInterval: undefined, upDimm: true, currentBrightness: Number(getState(actors[0]).val)};
     }
     
     if (obj.state.val === true){
-        cache[cacheKey].dimInterval = setInterval(()=>{
+        // Create interval
+        cache[cacheKey].dimInterval = setInterval(()=>{     
+            // Dim up       
             if (cache[cacheKey].upDimming){
                 cache[cacheKey].currentBrightness = cache[cacheKey].currentBrightness + 10;               
-            }        
+            }   
+            // Dim down     
             else{
                 cache[cacheKey].currentBrightness = cache[cacheKey].currentBrightness - 10;
             }
 
-            if (cache[cacheKey].currentBrightness >= 100){
+            // Check if the current value exceeds the maximum value
+            if (cache[cacheKey].currentBrightness > 100){
                 cache[cacheKey].currentBrightness = 100;
                 // Auto change dim direction 
                 //cache[cacheKey].upDimming = false;
             }
+            // Check if the current value is below the minimum value 
             else if (cache[cacheKey].currentBrightness <= 10){
-                cache[cacheKey].currentBrightness = 2;
+                cache[cacheKey].currentBrightness = 0;
                 // Auto change dim direction 
                 //cache[cacheKey].upDimming = true;
             }   
-
+            // Set state
             for (const key in actors) {
                 const dataPoint = getDataPoint(actors[key], 'level'); 
-                if  (dataPoint != undefined){    
-                    setState(dataPoint, Number(cache[cacheKey].currentBrightness));
+                if  (dataPoint != undefined){
+                    let briMin = 0, briMax = 100;
+                    // Get actor
+                    const actor = Actors.find(x=>x.name == actors[key]);
+                    // Has an override been defined?
+                    if (actor.briMin){
+                        briMin = actor.briMin;
+                    }
+                    // Has an override been defined?
+                    if (actor.briMax){
+                        briMax = actor.briMax;
+                    }
+                    // Create reMap 
+                    const reMap = createRemap(0 ,100, briMin, briMax);
+                    // Set state    
+                    setState(dataPoint, reMap(cache[cacheKey].currentBrightness));
                 }
             } 
         }, 500);
@@ -264,6 +317,56 @@ function autoDim(obj, actors){
         // On release change dim direction 
         cache[cacheKey].upDimming = !cache[cacheKey].upDimming;
     }
+}
+
+/**
+* @param {string[]} actors
+* @param {boolean} upDown
+*/
+function dimUpDown(actors, upDown){
+    // Check if the cache is available, if not one will be created 
+    const cacheKey = JSON.stringify(actors);
+    if (!cache[cacheKey]){
+        cache[cacheKey] = {currentBrightness: Number(getState(actors[0]).val)};
+    }
+    // Dim up   
+    if (upDown){
+        cache[cacheKey].currentBrightness = cache[cacheKey].currentBrightness + 10;
+    }
+    // Dim down  
+    else{
+        cache[cacheKey].currentBrightness = cache[cacheKey].currentBrightness - 10;
+    }
+    
+    // Check if the current value exceeds the maximum value
+    if (cache[cacheKey].currentBrightness <= 10){
+        cache[cacheKey].currentBrightness = 0;
+    }
+    // Check if the current value is below the minimum value 
+    else if (cache[cacheKey].currentBrightness >= 100){
+        cache[cacheKey].currentBrightness = 100;
+    }
+    // Set state
+    for (const key in actors) {
+        const dataPoint = getDataPoint(actors[key], 'level');   
+        if  (dataPoint != undefined){    
+            let briMin = 0, briMax = 100;
+            // Get actor
+            const actor = Actors.find(x=>x.name == actors[key]);
+            // Has an override been defined?
+            if (actor.briMin){
+                briMin = actor.briMin;
+            }
+            // Has an override been defined?
+            if (actor.briMax){
+                briMax = actor.briMax;
+            }
+            // Create reMap 
+            const reMap = createRemap(0 , 100, briMin, briMax);
+            // Set state
+            setState(dataPoint, reMap(cache[cacheKey].currentBrightness));
+        }
+    } 
 }
 
 /**
@@ -296,4 +399,18 @@ function openClose(actors, openClose){
             setState(dataPoint, Number(desiredPos));
         }
     } 
+}
+
+/**
+ * Create a function that maps a value to a range
+ * @param {Number} inMin Input range minimun value
+ * @param {Number} inMax Input range maximun value
+ * @param {Number} outMin Output range minimun value
+ * @param {Number} outMax Output range maximun value
+ * @return {function} A function that converts a value
+ */
+function createRemap(inMin, inMax, outMin, outMax) {
+    return function remaper(x) {
+        return Number(((x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin).toFixed());
+    };
 }
